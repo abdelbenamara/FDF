fcecb1b871ec022058972c6ba84663678ff6f85b
Abdel Benamara <abdelb.contact@gmail.com>
2023-07-07 02:19:12 +0200

    Updated fdf

--- fdf.h
+++ fdf.h
@@ -6,13 +6,14 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 23:00:16 by abenamar          #+#    #+#             */
-/*   Updated: 2023/07/06 23:02:24 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/07/07 02:11:38 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
+# include <fcntl.h>
 # include <math.h>
 # include <X11/Xlib.h>
 # include <X11/Xutil.h>
@@ -39,15 +40,19 @@ typedef struct s_xclient
 	void		*mlx;
 	void		*win;
 	t_ximage	*ximage;
+	char		***map;
 	int			ox;
 	int			oy;
 }	t_xclient;
+
+void		ft_free_map(char ***map);
+char		***ft_new_map(char *file);
 
 void		ft_free_ximage(t_ximage *ximage);
 t_ximage	*ft_new_ximage(t_xclient *xclient);
 
 void		ft_free_xclient(t_xclient *xclient);
-t_xclient	*ft_new_xclient(void);
+t_xclient	*ft_new_xclient(char ***map);
 
 int			ft_render_frame(t_xclient *xclient);
 
