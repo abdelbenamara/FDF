d6330dea4794eb403ea4e94b3a00f825ffe30abc
Abdel Benamara <abdelb.contact@gmail.com>
2023-07-07 02:20:03 +0200

    Updated fdf

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 22:59:33 by abenamar          #+#    #+#             */
-/*   Updated: 2023/07/05 23:58:49 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/07/07 02:15:11 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 int	main(int ac, char **av)
 {
+	char		***map;
 	t_xclient	*xclient;
 
 	if (ac < 2)
 		return (ft_dprintf(STDERR_FILENO, USAGE), 2);
-	(void) av;
-	xclient = ft_new_xclient();
-	if (!xclient)
+	map = ft_new_map(av[1]);
+	if (!map)
 		return (1);
+	xclient = ft_new_xclient(map);
+	if (!xclient)
+		return (ft_free_map(map), 1);
 	mlx_hook(xclient->win, \
 		DestroyNotify, ButtonReleaseMask, mlx_loop_end, xclient->mlx);
 	mlx_hook(xclient->win, \
