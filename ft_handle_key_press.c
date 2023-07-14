141ec9f8b918c461e7d708491f93a36f2e0ebb99
Abdel Benamara <abdelb.contact@gmail.com>
2023-07-14 02:41:35 +0200

    Updated ft_handle_key_press

--- ft_handle_key_press.c
+++ ft_handle_key_press.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 19:47:35 by abenamar          #+#    #+#             */
-/*   Updated: 2023/07/06 00:09:06 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/07/12 19:08:11 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 int	ft_handle_key_press(int keycode, t_xclient *xclient)
 {
-	if (keycode == XK_Up)
+	if (keycode == XK_w || keycode == XK_z)
 		xclient->oy -= YSIZE / 100;
-	else if (keycode == XK_Down)
+	else if (keycode == XK_s)
 		xclient->oy += YSIZE / 100;
-	else if (keycode == XK_Left)
+	else if (keycode == XK_a || keycode == XK_q)
 		xclient->ox -= XSIZE / 100;
-	else if (keycode == XK_Right)
+	else if (keycode == XK_d)
 		xclient->ox += XSIZE / 100;
 	else if (keycode == XK_Escape)
 		mlx_loop_end(xclient->mlx);
