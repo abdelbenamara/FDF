754103e91c3ec54a504d17035036bf3bb0548de8
Abdel Benamara <abdelb.contact@gmail.com>
2023-07-14 02:41:52 +0200

    Updated ft_render_frame

--- ft_render_frame.c
+++ ft_render_frame.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 20:22:29 by abenamar          #+#    #+#             */
-/*   Updated: 2023/07/06 23:21:48 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/07/12 21:20:19 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,41 +17,48 @@ static void	ft_put_pixel(t_ximage *ximage, int x, int y, int color)
 	char	*pos;
 
 	pos = ximage->data + (y * ximage->lsize + x * (ximage->bpp / 8));
-	*((unsigned int *) pos) = color;
+	*((unsigned int *) pos) = mlx_get_color_value(ximage->mlx, color);
 }
 
-static void	ft_draw_grid(t_ximage *ximage)
+static void	ft_draw_map(t_ximage *ximage, char ***map, size_t ppad)
 {
 	int	i;
 	int	j;
+	int	x;
+	int	y;
 
 	j = 0;
-	while (j <= YSIZE / 2)
+	y = 0;
+	while (map[j])
 	{
 		i = 0;
-		if (j % 32 == 0)
-			while (i <= XSIZE / 2)
-				(ft_put_pixel(ximage, i, j, 0x00FFFFFF), ++i);
-		while (i <= XSIZE / 2)
+		x = 0;
+		while (map[j][i])
 		{
-			if (i % 32 == 0)
-				ft_put_pixel(ximage, i, j, 0x00FFFFFF);
+			if (y % ppad == 0)
+				while (x < (int)(i * ppad))
+					(ft_put_pixel(ximage, x, y, 0x00FFFFFF), ++x);
+			else
+				(ft_put_pixel(ximage, x, y, 0x00FFFFFF), x += ppad);
 			++i;
 		}
-		++j;
+		if (y % ppad == 0)
+			++j;
+		++y;
 	}
 }
 
 int	ft_render_frame(t_xclient *xclient)
 {
-	t_ximage	*buf;
-	t_ximage	*tmp;
+	t_ximage		*buf;
+	t_ximage		*tmp;
 
 	buf = ft_new_ximage(xclient);
-	ft_draw_grid(buf);
+	ft_draw_map(buf, xclient->map, xclient->ppad);
 	tmp = xclient->ximage;
 	xclient->ximage = buf;
 	ft_free_ximage(tmp);
+	mlx_clear_window(xclient->mlx, xclient->win);
 	mlx_put_image_to_window(xclient->mlx, xclient->win, \
 		xclient->ximage->img, xclient->ox, xclient->oy);
 	return (EXIT_SUCCESS);
