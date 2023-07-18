a855dcc4c97804034ac84b6ebc6536370c83c9a9
Abdel Benamara <abdelb.contact@gmail.com>
2023-07-18 20:17:26 +0200

    Updated ft_free_xclient

--- ft_free_xclient.c
+++ ft_free_xclient.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 04:19:41 by abenamar          #+#    #+#             */
-/*   Updated: 2023/07/07 02:12:26 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/07/18 18:11:09 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	ft_free_xclient(t_xclient *xclient)
 {
-	ft_free_map(xclient->map);
-	if (xclient->ximage)
-		ft_free_ximage(xclient->ximage);
+	ft_free_map(xclient->xparams.map);
+	if (xclient->ximage.img)
+		mlx_destroy_image(xclient->mlx, xclient->ximage.img);
 	if (xclient->win)
 		mlx_destroy_window(xclient->mlx, xclient->win);
 	if (xclient->mlx)
