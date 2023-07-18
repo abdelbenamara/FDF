23d38352293d85dae0584008c1938c2d6b189c36
Abdel Benamara <abdelb.contact@gmail.com>
2023-07-18 20:17:08 +0200

    Updated fdf

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 23:00:16 by abenamar          #+#    #+#             */
-/*   Updated: 2023/07/14 02:36:23 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/07/18 20:14:44 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,37 +21,50 @@
 # include "libft.h"
 
 # define USAGE	"Usage: fdf file.fdf\n"
-# define XSIZE	1280
-# define YSIZE	720
+# define WIDTH	1280
+# define HEIGHT	720
 # define TITLE	"FDF"
+
+typedef struct s_vec2
+{
+	int	x;
+	int	y;
+}	t_vec2;
+
+typedef struct s_xparams
+{
+	char	***map;
+	size_t	width;
+	size_t	height;
+	size_t	wpad;
+	size_t	hpad;
+	size_t	padding;
+	size_t	zoom;
+	t_vec2	origin;
+}	t_xparams;
 
 typedef struct s_ximage
 {
-	void	*mlx;
-	void	*img;
-	char	*data;
-	int		bpp;
-	int		lsize;
-	int		endian;
+	void		*mlx;
+	void		*img;
+	char		*data;
+	int			bpp;
+	int			lsize;
+	int			endian;
+	t_xparams	xparams;
 }	t_ximage;
 
 typedef struct s_xclient
 {
 	void		*mlx;
 	void		*win;
-	t_ximage	*ximage;
-	char		***map;
-	size_t		ppad;
-	int			ox;
-	int			oy;
+	t_xparams	xparams;
+	t_ximage	ximage;
 }	t_xclient;
 
 void		ft_free_map(char ***map);
 char		***ft_new_map(char *file);
-
-void		ft_free_ximage(t_ximage *ximage);
-t_ximage	*ft_new_ximage(t_xclient *xclient);
-
+t_ximage	ft_init_ximage(t_xclient *xclient);
 void		ft_free_xclient(t_xclient *xclient);
 t_xclient	*ft_new_xclient(char ***map);
 
