72526ffc6673cf570da21415133e7ef71a9930c6
Abdel Benamara <abdelb.contact@gmail.com>
2023-07-21 02:38:56 +0200

    Updated ft_render_frame

--- ft_render_frame.c
+++ ft_render_frame.c
@@ -6,62 +6,30 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 20:22:29 by abenamar          #+#    #+#             */
-/*   Updated: 2023/07/18 20:15:35 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/07/21 02:31:56 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static void	ft_put_pixel(t_ximage ximage, int x, int y, int color)
+static t_vec2	ft_2d_projection(t_ximage ximage, int x, int y, int z)
 {
-	t_xparams	xparams;
-	char		*pos;
+	const double	tx = ximage.xparams.theta.x * M_PI / 180;
+	const double	ty = ximage.xparams.theta.y * M_PI / 180;
+	const double	tz = ximage.xparams.theta.z * M_PI / 180;
+	t_vec3			s;
+	t_vec2			b;
 
-	xparams = ximage.xparams;
-	x += xparams.origin.x - xparams.zoom * xparams.width / 2;
-	y += xparams.origin.y - xparams.zoom * xparams.height / 2;
-	if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
-		return ;
-	pos = ximage.data + x * (ximage.bpp / 8) + y * ximage.lsize;
-	*((unsigned int *) pos) = mlx_get_color_value(ximage.mlx, color);
+	s.x = (x - ximage.xparams.width / 2) * ximage.xparams.scale.x;
+	s.y = (y - ximage.xparams.height / 2) * ximage.xparams.scale.x;
+	s.z = z * ximage.xparams.scale.y;
+	b.x = cos(ty) * (cos(tz) * s.x + sin(tz) * s.y) - sin(ty) * s.z;
+	b.y = sin(tx) * (sin(ty) * (cos(tz) * s.x + sin(tz) * s.y) + cos(ty) * s.z);
+	b.y -= cos(tx) * (sin(tz) * s.x - cos(tz) * s.y);
+	return (b);
 }
 
-static t_vec2	ft_get_projection(t_ximage ximage, int x, int y, int z)
-{
-	t_vec2		u;
-
-	(void) z;
-	u.x = x * ximage.xparams.zoom;
-	u.y = y * ximage.xparams.zoom;
-	return (u);
-}
-
-static void	ft_draw_line(t_ximage ximage, t_vec2 u, t_vec2 v)
-{
-	const int	dx = v.x - u.x;
-	const int	dy = v.y - u.y;
-	int			d;
-	t_vec2		w;
-
-	d = 2 * dy - dx;
-	w.x = u.x;
-	w.y = u.y;
-	while (w.x < v.x || w.y < v.y)
-	{
-		ft_put_pixel(ximage, w.x, w.y, 0x00FFFFFF);
-		if (d > 0)
-		{
-			if (w.y < v.y)
-				++w.y;
-			d -= 2 * dx;
-		}
-		d += 2 * dy;
-		if (w.x < v.x)
-			++w.x;
-	}
-}
-
-static void	ft_draw_map(t_ximage ximage, char ***map)
+static void	ft_plot_map(t_ximage ximage, char ***map)
 {
 	int		i;
 	int		j;
@@ -74,15 +42,16 @@ static void	ft_draw_map(t_ximage ximage, char ***map)
 		i = 0;
 		while (map[j][i])
 		{
-			u = ft_get_projection(ximage, i, j, ft_atoi(map[j][i]));
-			ft_put_pixel(ximage, u.x, u.y, 0x00FFFFFF);
+			if (!(map[j][i]))
+				break ;
+			u = ft_2d_projection(ximage, i, j, ft_atoi(map[j][i]));
 			v = u;
 			if (i > 0)
-				u = ft_get_projection(ximage, i - 1, j, ft_atoi(map[j][i - 1]));
-			ft_draw_line(ximage, u, v);
-			if (j > 0)
-				u = ft_get_projection(ximage, i, j - 1, ft_atoi(map[j - 1][i]));
-			ft_draw_line(ximage, u, v);
+				v = ft_2d_projection(ximage, i - 1, j, ft_atoi(map[j][i - 1]));
+			ft_plot_line(ximage, u, v);
+			if (j > 0 && map[j - 1][i])
+				v = ft_2d_projection(ximage, i, j - 1, ft_atoi(map[j - 1][i]));
+			ft_plot_line(ximage, u, v);
 			++i;
 		}
 		++j;
@@ -92,15 +61,15 @@ static void	ft_draw_map(t_ximage ximage, char ***map)
 int	ft_render_frame(t_xclient *xclient)
 {
 	t_ximage	xbuffer;
-	void		*old_img;
+	void		*oldimg;
 
 	xbuffer = ft_init_ximage(xclient);
 	if (!(xbuffer.img))
 		return (EXIT_FAILURE);
-	ft_draw_map(xbuffer, xclient->xparams.map);
-	old_img = xclient->ximage.img;
+	ft_plot_map(xbuffer, xclient->xparams.map);
+	oldimg = xclient->ximage.img;
 	xclient->ximage = xbuffer;
-	mlx_destroy_image(xclient->mlx, old_img);
+	mlx_destroy_image(xclient->mlx, oldimg);
 	mlx_clear_window(xclient->mlx, xclient->win);
 	mlx_put_image_to_window(xclient->mlx, \
 		xclient->win, xclient->ximage.img, 0, 0);
