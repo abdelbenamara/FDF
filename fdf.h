ae56f86db40dd0631646900651310a9cdb64b3ee
Abdel Benamara <abdelb.contact@gmail.com>
2023-07-22 01:32:22 +0200

    Updated fdf

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 23:00:16 by abenamar          #+#    #+#             */
-/*   Updated: 2023/07/21 02:30:20 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/07/22 00:37:56 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ typedef struct s_vec3
 
 typedef struct s_xparams
 {
-	char	***map;
+	int		***map;
 	size_t	width;
 	size_t	height;
 	int		basis;
@@ -72,15 +72,19 @@ typedef struct s_xclient
 	t_ximage	ximage;
 }	t_xclient;
 
-void		ft_free_map(char ***map);
-char		***ft_new_map(char *file);
+void		ft_free_tab(void **tab);
+size_t		ft_tab_size(void **tab);
+void		ft_free_map(int ***map);
+int			ft_atoi_base(const char *nptr, const char *base);
+int			***ft_new_map(char *file);
+
 void		ft_reset_xparams(t_xparams *xparams);
 t_ximage	ft_init_ximage(t_xclient *xclient);
 void		ft_free_xclient(t_xclient *xclient);
-t_xclient	*ft_new_xclient(char ***map);
+t_xclient	*ft_new_xclient(int ***map);
 
 void		ft_put_pixel(t_ximage ximage, int x, int y, int color);
-void		ft_plot_line(t_ximage ximage, t_vec2 u, t_vec2 v);
+void		ft_plot_line(t_ximage ximage, t_vec2 u, t_vec2 v, int color);
 int			ft_render_frame(t_xclient *xclient);
 
 int			ft_handle_key_press(int keycode, t_xclient *xclient);
