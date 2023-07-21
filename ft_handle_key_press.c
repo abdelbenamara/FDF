e49e579588aa8b846eb7d69ee3d04de6e2c91dcc
Abdel Benamara <abdelb.contact@gmail.com>
2023-07-22 01:34:02 +0200

    Updated ft_handle_key_press

--- ft_handle_key_press.c
+++ ft_handle_key_press.c
@@ -6,26 +6,45 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 19:47:35 by abenamar          #+#    #+#             */
-/*   Updated: 2023/07/21 02:30:49 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/07/22 01:27:33 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+static void	ft_set_view_angle(t_xclient *xclient, int tx, int ty, int tz)
+{
+	xclient->xparams.theta.x = tx;
+	xclient->xparams.theta.y = ty;
+	xclient->xparams.theta.z = tz;
+}
+
 static void	ft_handle_rotation(int keycode, t_xclient *xclient)
 {
-	if (keycode == XK_w)
-		ft_printf("theta.x = %d\n", ++(xclient->xparams.theta.x));
+	if (keycode == XK_1)
+		ft_set_view_angle(xclient, -35, 0, -45);
+	else if (keycode == XK_2)
+		ft_set_view_angle(xclient, -35, 0, -135);
+	else if (keycode == XK_3)
+		ft_set_view_angle(xclient, -35, 0, 135);
+	else if (keycode == XK_4)
+		ft_set_view_angle(xclient, -35, 0, 45);
+	else if (keycode == XK_9)
+		ft_set_view_angle(xclient, -90, 0, 0);
+	else if (keycode == XK_0)
+		ft_set_view_angle(xclient, 0, 0, 0);
+	else if (keycode == XK_w)
+		++(xclient->xparams.theta.x);
 	else if (keycode == XK_s)
-		ft_printf("theta.x = %d\n", --(xclient->xparams.theta.x));
+		--(xclient->xparams.theta.x);
 	else if (keycode == XK_a)
-		ft_printf("theta.z = %d\n", ++(xclient->xparams.theta.z));
+		++(xclient->xparams.theta.z);
 	else if (keycode == XK_d)
-		ft_printf("theta.z = %d\n", --(xclient->xparams.theta.z));
+		--(xclient->xparams.theta.z);
 	else if (keycode == XK_e)
-		ft_printf("theta.y = %d\n", ++(xclient->xparams.theta.y));
+		++(xclient->xparams.theta.y);
 	else if (keycode == XK_q)
-		ft_printf("theta.y = %d\n", --(xclient->xparams.theta.y));
+		--(xclient->xparams.theta.y);
 }
 
 int	ft_handle_key_press(int keycode, t_xclient *xclient)
@@ -41,13 +60,13 @@ int	ft_handle_key_press(int keycode, t_xclient *xclient)
 	else if (keycode == XK_Right)
 		xclient->xparams.origin.x += WIDTH / 64;
 	else if (keycode == XK_i)
-		ft_printf("scale.x = %d\n", ++(xclient->xparams.scale.x));
+		++(xclient->xparams.scale.x);
 	else if (keycode == XK_o)
-		ft_printf("scale.x = %d\n", --(xclient->xparams.scale.x));
+		--(xclient->xparams.scale.x);
 	else if (keycode == XK_k)
-		ft_printf("scale.y = %d\n", ++(xclient->xparams.scale.y));
+		++(xclient->xparams.scale.y);
 	else if (keycode == XK_l)
-		ft_printf("scale.y = %d\n", --(xclient->xparams.scale.y));
+		--(xclient->xparams.scale.y);
 	else if (keycode == XK_Escape)
 		mlx_loop_end(xclient->mlx);
 	else
