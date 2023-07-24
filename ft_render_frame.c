4f02e9673d404c8e59ba4bf9810d6a9b72271499
Abdel Benamara <abdelb.contact@gmail.com>
2023-07-24 10:08:11 +0200

    Updated ft_render_frame

--- ft_render_frame.c
+++ ft_render_frame.c
@@ -6,29 +6,30 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 20:22:29 by abenamar          #+#    #+#             */
-/*   Updated: 2023/07/22 00:38:36 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/07/24 10:04:58 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static t_vec2	ft_2d_projection(t_ximage ximage, int x, int y, int z)
+static t_vec3	ft_2d_projection(t_ximage ximage, int x, int y, int *z)
 {
 	const double	tx = ximage.xparams.theta.x * M_PI / 180;
 	const double	ty = ximage.xparams.theta.y * M_PI / 180;
 	const double	tz = ximage.xparams.theta.z * M_PI / 180;
-	t_vec3			s;
-	t_vec2			b;
+	t_vec3			a;
+	t_vec3			b;
 
-	s.x = (x - ximage.xparams.width / 2) * ximage.xparams.scale.x;
-	s.y = (y - ximage.xparams.height / 2) * ximage.xparams.scale.x;
-	s.z = z * ximage.xparams.scale.y;
-	b.x = cos(ty) * (cos(tz) * s.x + sin(tz) * s.y) - sin(ty) * s.z
+	a.x = (x - ximage.xparams.width / 2) * ximage.xparams.scale.x;
+	a.y = (y - ximage.xparams.height / 2) * ximage.xparams.scale.x;
+	a.z = z[0] * ximage.xparams.scale.y;
+	b.x = cos(ty) * (cos(tz) * a.x + sin(tz) * a.y) - sin(ty) * a.z
 		+ ximage.xparams.origin.x;
 	b.y = sin(tx) \
-		* (sin(ty) * (cos(tz) * s.x + sin(tz) * s.y) + cos(ty) * s.z) \
-		- cos(tx) * (sin(tz) * s.x - cos(tz) * s.y)
+		* (sin(ty) * (cos(tz) * a.x + sin(tz) * a.y) + cos(ty) * a.z) \
+		- cos(tx) * (sin(tz) * a.x - cos(tz) * a.y)
 		+ ximage.xparams.origin.y;
+	b.z = z[1];
 	return (b);
 }
 
@@ -36,8 +37,8 @@ static void	ft_plot_map(t_ximage ximage, int ***map)
 {
 	int		i;
 	int		j;
-	t_vec2	u;
-	t_vec2	v;
+	t_vec3	u;
+	t_vec3	v;
 
 	j = 0;
 	while (map[j])
@@ -47,14 +48,14 @@ static void	ft_plot_map(t_ximage ximage, int ***map)
 		{
 			if (!(map[j][i]))
 				break ;
-			u = ft_2d_projection(ximage, i, j, map[j][i][0]);
+			u = ft_2d_projection(ximage, i, j, map[j][i]);
 			v = u;
 			if (i > 0)
-				v = ft_2d_projection(ximage, i - 1, j, map[j][i - 1][0]);
-			ft_plot_line(ximage, u, v, map[j][i][1]);
+				v = ft_2d_projection(ximage, i - 1, j, map[j][i - 1]);
+			ft_plot_line(ximage, u, v);
 			if (j > 0 && map[j - 1][i])
-				v = ft_2d_projection(ximage, i, j - 1, map[j - 1][i][0]);
-			ft_plot_line(ximage, u, v, map[j][i][1]);
+				v = ft_2d_projection(ximage, i, j - 1, map[j - 1][i]);
+			ft_plot_line(ximage, u, v);
 			++i;
 		}
 		++j;
